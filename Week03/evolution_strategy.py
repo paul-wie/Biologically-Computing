d91c1d561fd984c0971131830cbf293874feec2d
Paul Wieland <wieland.paul@gmx.net>
2018-09-24 01:08:41 +0200

    survior creation

--- Week03/evolution_strategy.py
+++ Week03/evolution_strategy.py
@@ -8,11 +8,20 @@ def parent_selection(population):
     random2 = rand.randint(1,length-1)
     return population[random1], population[random2]
 
-
+#survivor creation
+#select randomly two parents and create one new child
+#return 'number_of_survivor' survivor
+def survivor_creation(population, number_of_survivor):
+    survivor = []
+    while number_of_survivor > 0:
+        parents = parent_selection(population)
+        survivor.append( (parents[0]+parents[1]) /2 )
+        number_of_survivor -=1
+    return survivor
 
 
 def main():
     population = [1,2,3,4]
-    print(parent_selection(population))
+    print(survivor_creation(population,8))
 
 main()
