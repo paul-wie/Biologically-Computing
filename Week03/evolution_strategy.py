89abf0d0b4119aa9acaca9d7c465fc8ac06a772b
Paul Wieland <wieland.paul@gmx.net>
2018-09-24 01:02:02 +0200

    parent selection

--- Week03/evolution_strategy.py
+++ Week03/evolution_strategy.py
@@ -1,3 +1,18 @@
+import random as rand
 
-minimum = 1
-maximum = 4 
+#parent selection, uniform random
+#return: two randomly selected parents
+def parent_selection(population):
+    length = len(population)
+    random1 = rand.randint(1,length-1)
+    random2 = rand.randint(1,length-1)
+    return population[random1], population[random2]
+
+
+
+
+def main():
+    population = [1,2,3,4]
+    print(parent_selection(population))
+
+main()
