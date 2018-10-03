53ab16ee85343d886ab1c41718aeb566e1dd9975
Paul Wieland <wieland.paul@gmx.net>
2018-10-03 23:42:17 +0200

    mlp

--- assignment_2/code/mlp.py
+++ assignment_2/code/mlp.py
@@ -9,19 +9,23 @@ class mlp:
         # type(inputs) = type(targets) = <class 'numpy.ndarray'>
         #so we need numpy to modify the arrays
         # height: 41, width: 12 (nhidden = 12)
-        self.weight_matrix_1 = create_random_array(len(inputs[0])+1,nhidden)
+        self.weight_matrix_1 = create_random_matrix(len(inputs[0])+1,nhidden)
         #height 13, width: 8 (nhidden =12)
-        self.weight_matrix_2 = create_random_array(nhidden+1,len(targets[0]))
+        self.weight_matrix_2 = create_random_matrix(nhidden+1,len(targets[0]))
 
         print('To be implemented')
 
 
 
     def earlystopping(self, inputs, targets, valid, validtargets):
-
+        self.train(1,2)
         print('To be implemented')
 
     def train(self, inputs, targets, iterations=100):
+        while iterations > 0:
+            print(iterations)
+            iterations -=1
+
         print('To be implemented')
 
     def forward(self, inputs):
