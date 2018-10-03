c8ec3f1bf066e1555e16d315480324a9b80d32d5
Paul Wieland <wieland.paul@gmx.net>
2018-10-03 23:06:53 +0200

    array operations

--- assignment_2/code/array_operations.py
+++ assignment_2/code/array_operations.py
@@ -1,5 +1,25 @@
 import numpy as np
+import sys
+import random as rand
 
 #create a random array with dimension og height and width
-def create_random_array(height, width):
-    return np.random.rand(height,width)
+def create_random_matrix(height, width):
+    output = []
+    for index1 in range(height):
+        new_row = []
+        for index2 in range(width):
+            new_row.append(rand.uniform(0,1))
+        output.append(new_row)
+    return np.array(output)
+
+#vector matrix multiplication
+def vec_matr_mult(vec,matr):
+    if len(vec) != len(matr):
+        print("Vector and matrx do not have the same dimension")
+        sys.exit(0)
+    else:
+        output = np.zeros(len(matr[0]))
+        for index in range(len(matr[0])):
+            for mul in range(len(vec)):
+                output[index] += vec[mul]*matr[mul][index]
+        return output
--- assignment_2/code/test_arrays.py
+++ assignment_2/code/test_arrays.py
@@ -2,22 +2,40 @@ from array_operations import *
 
 #good
 def test_creat_random_array():
-    a1 = create_random_array(4,3)
+    a1 = create_random_matrix(4,3)
     print(a1)
     print("height:", len(a1))
     print("width:",len(a1[0]))
     print(type(a1))
     print("---------------------")
-    a2 = create_random_array(5,7)
+    a2 = create_random_matrix(4,6)
     print(a2)
     print("height:", len(a2))
     print("width:",len(a2[0]))
     print(type(a2))
+#good
+def test_vector_matrix_multiplication():
+    v1 = np.array([1,2,3])
+    m1 = np.array([[1,2,3],
+                    [1,2,3],
+                    [1,2,3]
+                  ])
+    print(vec_matr_mult(v1,m1))
+    print("---------------------------")
+    m2 = create_random_matrix(3,5)
+    v2 = np.array([1,2,3])
+    res1 = vec_matr_mult(v2,m2)
+    res2 = v2.dot(m2)
+    print(m2)
+    print("Res1:",res1)
+    print("Res2:",res2)
 
 
 
 def main():
-    test_creat_random_array()
+    #test_creat_random_array()
+    test_vector_matrix_multiplication()
+
 
 
 main()
